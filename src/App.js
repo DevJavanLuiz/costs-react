17b54c5ab6edeb37b3b49c2186f8f04eb853141f
javan <javanluiz757@gmail.com>
2024-07-29 12:32:50 -0300

    building page form

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import Company from "./pages/Company";
-import NewProject from "./pages/NewProject";
-import Container from "./layout/Container.js";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject.js";
+import Container from "./components/layout/Container.js";
+import Footer from "./components/layout/Footer.js";
+import NavBar from "./components/layout/NavBar.js";
+import Projects from "./components/pages/Projects.js";
 
 function App() {
   return (
     <Router>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contato</Link>
-        </li>
-        <li>
-          <Link to="/company">Company</Link>
-        </li>
-        <li>
-          <Link to="/newproject">NewProject</Link>
-        </li>
-      </ul>
-      <Container>
+      <NavBar />
+      <Container customClass="min-height">
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
           <Route path="/company" element={<Company />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
           <Route path="/newproject" element={<NewProject />}></Route>
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
--- src/pages/NewProject.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NewProject() {
-  return (
-    <>
-      <h1>Estou na NewProject</h1>
-    </>
-  );
-}
