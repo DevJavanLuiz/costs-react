57456781faa642393ea2ee4529955779723ea948
javan <javanluiz757@gmail.com>
2024-08-02 17:05:52 -0300

    page edit project

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -2,25 +2,103 @@ import styles from "./Project.module.css";
 
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import Loading from "../layout/Loading";
+import Container from "../layout/Container";
+import ProjectForm from "../project/ProjectForm";
+import Message from "../layout/Message";
 
 export default function Project() {
   const { id } = useParams();
 
   const [project, setProject] = useState([]);
+  const [message, setMessage] = useState();
+  const [type, setType] = useState();
+  const [showProjectForm, setShowProjectForm] = useState(false);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/projects/${id}`, {
-      method: "GET",
+    setTimeout(() => {
+      fetch(`http://localhost:5000/projects/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          setProject(data);
+        })
+        .catch((err) => console.log(err));
+    }, 500);
+  }, [id]);
+
+  function toggleProjectForm() {
+    setShowProjectForm(!showProjectForm);
+  }
+
+  function editPost(project) {
+    // budget validation
+    if (project.budget < project.cost) {
+      setMessage("Orçamento menor que o custo total do projeto.");
+      setType("error");
+      return false;
+    }
+
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: "PATCH",
       headers: {
         "Content-Type": "application/json",
       },
+      body: JSON.stringify(project),
     })
       .then((resp) => resp.json())
       .then((data) => {
         setProject(data);
+        setShowProjectForm(false);
+        setMessage("Projeto editado com sucesso!");
+        setType("success");
       })
       .catch((err) => console.log(err));
-  }, [id]);
+  }
 
-  return <p>{project.name}</p>;
+  return (
+    <>
+      {project.name ? (
+        <div className={styles.project_details}>
+          <Container customClass="column">
+            {message && <Message type={type} msg={message} />}
+            <div className={styles.details_container}>
+              <h1>Projeto: {project.name}</h1>
+              <button className={styles.btn} onClick={toggleProjectForm}>
+                {!showProjectForm ? "Editar projeto" : "Fechar"}
+              </button>
+              {!showProjectForm ? (
+                <div className={styles.project_info}>
+                  <p>
+                    <span>Categoria: </span>
+                    {project.category.name}
+                  </p>
+                  <p>
+                    <span>Oraçamento Total: </span>R${project.budget}
+                  </p>
+                  <p>
+                    <span>Total Utilizado: </span>R${project.cost}
+                  </p>
+                </div>
+              ) : (
+                <div className={styles.project_info}>
+                  <ProjectForm
+                    handleSubmit={editPost}
+                    btnText="Finalizar edição"
+                    projectData={project}
+                  />
+                </div>
+              )}
+            </div>
+          </Container>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
 }
